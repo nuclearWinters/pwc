1f864b3cf69891a5e7f1f2e7c7ba78a0f5596628
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2019-03-24 15:41:10 -0500

    changes1

--- src/App.js
+++ src/App.js
@@ -5,21 +5,39 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>Páginas Web en Chetumal</div>
+        <di>PLANES DE DISEÑO WEB</di>
+        <div>
+          <div>Plan Página Informativa con código propio</div>
+          <div>Este plan es perfecto para paginas web sin mucho contenido dinamico, es decir, que su base de datos no requiere atención especial y en algún momento se quiera escalar.</div>
+          <div>$3,000 MXN</div>
+          <div>Hosting <strong>gratis</strong> siguiendo las reglas de Firebase Hosting</div>
+          <div>Hosting de base de datos NoSQL <strong>gratis</strong> siguiendo las reglas de Firebase Hosting</div>
+          <div>Dominio comprado con GoDaddy (Min. $20 MXN el primer año)</div>
+          <div>Tecnologias: ReactJS</div>
+          <div>Estructura ideal para poder aparecer en los primeros resultados de Google</div>
+          <div>Diseño diferente para mobiles y monitores</div>
+          <div>Plan Página Informativa con Wordpress</div>
+          <div>Este plan es perfecto para paginas web sin mucho contenido dinamico y que requieran el CMS Wordpress en su desarrollo</div>
+          <div>$3,000 MXN</div>
+          <div>Hosting de <strong>$60 MXN mensuales</strong> en Bluehost</div>
+          <div>Hosting de base de datos NoSQL <strong>gratis</strong> siguiendo las reglas de Firebase Hosting</div>
+          <div>Dominio comprado con GoDaddy (Min. $20 MXN el primer año)</div>
+          <div>Tecnologias: Wordpress</div>
+          <div>Estructura ideal para poder aparecer en los primeros resultados de Google</div>
+          <div>Diseño diferente para mobiles y monitores</div>
+          <div>$3,000 MXN</div>
+          <div>Plan Aplicación Web</div>
+          <div>Este plan es especial para páginas web dinamicas, es decir, que requieren planificar mejor que tipo de base de datos usar asi como su arquitectura.</div>
+          <div>Hosting a escoger (Firebase, Heroku, Amazon AWS, etc.) que pueden ser servicios gratuitos o de pago mensual</div>
+          <div>Hosting de base de datos y back-end a escoger (Firebase, Heroku, Amazon AWS, etc.) que pueden ser servicios gratuitos o de pago mensual</div>
+          <div>Compra de dominio a escoger (GoDaddy, Google Domains, etc.)</div>
+          <div>Tecnologias: Javascript Frameworks, MVC Frameworks, PHP, MySQL, MongoDB, Python, NodeJS, C#</div>
+          <div>Estructura ideal para poder aparecer en los primeros resultados de Google</div>
+          <div>Diseño diferente para mobiles y monitores</div>
+          <div>$6,000</div>
+        </div>
       </div>
     );
   }
